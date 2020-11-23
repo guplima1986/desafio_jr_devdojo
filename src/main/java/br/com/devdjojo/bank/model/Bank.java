05e7cd42483b5962712eab19ced80c9ad1d77fa4
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-23 12:22:37 +0000

    create the entities based in the JSON

---
+++ src/main/java/br/com/devdjojo/bank/model/Bank.java
@@ -0,0 +1,9 @@
+package br.com.devdjojo.bank.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Bank extends AbstractEntity{
+
+
+}
---
+++ src/main/java/br/com/devdjojo/bank/model/TransferDetails.java
@@ -0,0 +1,7 @@
+package br.com.devdjojo.bank.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class TransferDetails extends AbstractEntity{
+}
